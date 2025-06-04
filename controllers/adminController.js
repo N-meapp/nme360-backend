540edcbc3bde5b89ed25c38023f26f413bc65cc0
N-meapp <developersnme@gmail.com>
2025-06-04 18:16:09 +0530

    server

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,7 @@
 import { login, addBlogs, editBlog, listBlogs, listCareers, addCareers, deleteBlog, deleteCareer, editCareer, createEnquiry, listEnquiries } from "../handlers/adminHandler.js";
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import { response } from "express";
 dotenv.config();
 
 
@@ -183,33 +184,69 @@ export const deleteCareerController = async (req, res, next) => {
 
 
 export const sendMail = (req, res) => {
-    const { fullName, email, number, description, position } = req.body;
+    const { fullName, email, number, description, position, status } = req.body;
     const resumeFile = req.file;
 
-    if (!resumeFile) {
-        return res.status(400).json({ error: 'Resume file is required.' });
-    }
+    if (status == 'enquiry') {
+        const COMPANY_MAIL_ID = process.env.VITE_MAIL_ID
+        const COMPANY_MAIL_PASSWORD = process.env.VITE_APP_PASSWORD
 
-    // const COMPANY_MAIL_ID = 'hrnmeapp24@gmail.com';
-    // const COMPANY_MAIL_PASSWORD = 'nbia kktg datj vxfd';
-    
-    const COMPANY_MAIL_ID = process.env.VITE_MAIL_ID
-    const COMPANY_MAIL_PASSWORD = process.env.VITE_APP_PASSWORD
-    
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: COMPANY_MAIL_ID,
-            pass: COMPANY_MAIL_PASSWORD,
-        },
-    });
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: COMPANY_MAIL_ID,
+                pass: COMPANY_MAIL_PASSWORD,
+            },
+        });
 
-    const mailOptions = {
-        from: email,
-        to: COMPANY_MAIL_ID,
-        subject: `Job Application from ${fullName}`,
-        html: `
+        const mailOptions = {
+            from: email,
+            to: COMPANY_MAIL_ID,
+            subject: `Job Application from ${fullName}`,
+            html: `
+      <div style="font-family: Arial, sans-serif; color: #333;">
+        <h2>Enquiry about ${position}</h2>
+        <p><strong>Name:</strong> ${fullName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${number}</p>
+        <p><strong>Service:</strong> ${position}</p>
+        <p><strong>Description:</strong> ${description}</p>
+      </div>
+    `,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                return res.status(500).json({ error: 'Email not sent. Please try again.' });
+            }
+            res.status(200).json({ success: true, message: 'Email sent successfully' });
+        });
+    } else {
+
+        if (!resumeFile) {
+            return res.status(400).json({ error: 'Resume file is required.' });
+        }
+
+
+        const COMPANY_MAIL_ID = process.env.VITE_MAIL_ID
+        const COMPANY_MAIL_PASSWORD = process.env.VITE_APP_PASSWORD
+
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: COMPANY_MAIL_ID,
+                pass: COMPANY_MAIL_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: email,
+            to: COMPANY_MAIL_ID,
+            subject: `Job Application from ${fullName}`,
+            html: `
       <div style="font-family: Arial, sans-serif; color: #333;">
         <h2>New Job Application</h2>
         <p><strong>Name:</strong> ${fullName}</p>
@@ -219,21 +256,22 @@ export const sendMail = (req, res) => {
         <p><strong>Description:</strong> ${description}</p>
       </div>
     `,
-        attachments: [
-            {
-                filename: resumeFile.originalname,
-                content: resumeFile.buffer,           // ✅ attach the buffer
-            },
-        ],
-    };
+            attachments: [
+                {
+                    filename: resumeFile.originalname,
+                    content: resumeFile.buffer,           // ✅ attach the buffer
+                },
+            ],
+        };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-            return res.status(500).json({ error: 'Email not sent. Please try again.' });
-        }
-        res.status(200).json({ success: true, message: 'Email sent successfully' });
-    });
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                return res.status(500).json({ error: 'Email not sent. Please try again.' });
+            }
+            res.status(200).json({ success: true, message: 'Email sent successfully' });
+        });
+    }
 };
 
 
@@ -242,9 +280,40 @@ export const postEnquiryController = async (req, res, next) => {
 
     console.log(req.body, 'dfdfd');
 
-    const { name,email,number,subject,message } = req.body
+    const { name, email, number, subject, message } = req.body
 
-    const response = await createEnquiry({ name,email,number,subject,message })
+    let isValidName = true
+    let isValidEmail = true
+    let isValidPhone = true
+
+    if (!name) {
+        isValidName = false
+    } else if (!/^[A-Za-z\s]+$/.test(name)) {
+        isValidName = false
+    } else {
+        isValidName = true
+    }
+    if (!email) {
+        isValidEmail = false
+    } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+        isValidEmail = false
+    } else {
+        isValidEmail = true
+    }
+    if (!number) {
+        isValidPhone = false
+    } else if (!/^(?:\+?\d{1,3}[-.\s]?)?(?:\(?\d{3}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}$/.test(number)) {
+        isValidPhone = false
+    } else {
+        isValidPhone = true
+    }
+
+    let response;
+    if (isValidEmail && isValidEmail && isValidPhone) {
+        response = await createEnquiry({ name, email, number, subject, message })
+    } else {
+        response = null
+    }
 
 
     if (response) {
